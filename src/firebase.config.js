93fa7a8774c0b11e41f91709e2fd907064d4986a
arifulislam-com <arifulislam9565@gmail.com>
2020-04-09 09:44:11 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAa5UbMyMRp0B0KNqfb6SgaoVNZItQllYw",
+    authDomain: "ema-john-simple-20f9e.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-20f9e.firebaseio.com",
+    projectId: "ema-john-simple-20f9e",
+    storageBucket: "ema-john-simple-20f9e.appspot.com",
+    messagingSenderId: "1043856797464",
+    appId: "1:1043856797464:web:6f6d07532207f6e540786d"
+  };
+
+  export default firebaseConfig;
